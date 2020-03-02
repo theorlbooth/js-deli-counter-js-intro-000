be0d8554debbc6c819ae40f4aee5ff3cbb604a09
Theo Booth <theorlbooth@googlemail.com>
2020-03-02 12:50:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ function nowServing(katzDeliLine) {
       return `Currently serving ${katzDeliLine.shift()}.`
   }
 }
+
+function currentLine(current_Line) {
+  var line = ["The line is curently: "]
+  if (current_Line === 0) {
+    return "The line is empty."
+  } else {
+    for (let i = 0; i < current_Line.length+1; i += 1) {
+      line.push(`${i}. ${current_Line[i]}, `)
+    }
+  return line
+  }
+}
