5c3c2acf247c69c4335ca6e5823b99e60061e479
Theo Booth <theorlbooth@googlemail.com>
2020-03-02 12:51:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(katzDeliLine) {
 function currentLine(current_Line) {
   var line = ["The line is curently: "]
   if (current_Line.length === 0) {
-    return "The line is empty."
+    return "The line is currently empty."
   } else {
     for (let i = 0; i < current_Line.length+1; i += 1) {
       line.push(`${i}. ${current_Line[i]}, `)
