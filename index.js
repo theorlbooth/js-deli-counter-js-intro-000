7a9e0bfc3fb3952b523a2af0691958e4f266578d
Theo Booth <theorlbooth@googlemail.com>
2020-03-02 12:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function takeANumber(currentLine, customers_name) {
   currentLine.push(customers_name)
   return `Welcome, ${customers_name}. You are number ${currentLine.length} in line.`
 }
+
+function nowServing(katzDeliLine) {
+  if katzDeliLine.length = 0 {
+    return "There is nobody waiting to be served!"
+  } else {
+    return katzDeliLine.shift()
+  }
+}
