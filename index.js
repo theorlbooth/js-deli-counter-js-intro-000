7a11646747b846a609e5207b95d05d47080255b5
Theo Booth <theorlbooth@googlemail.com>
2020-03-02 12:36:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function nowServing(katzDeliLine) {
   if katzDeliLine.length = 0 {
     return "There is nobody waiting to be served!"
   } else {
-    return katzDeliLine.shift()
+      return katzDeliLine.shift()
   }
 }
